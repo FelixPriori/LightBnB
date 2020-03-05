1bb7dea8430e9be52c9cf1c62c1eb806a2396f5d
FelixPriori <felixriouxsabourin@gmail.com>
2020-03-05 14:00:04 +0000

    Implemented helper function to get all properties

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,5 +1,14 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'lightbnb',
+});
+
 
 /// Users
 
@@ -67,11 +76,16 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
+  // const limitedProperties = {};
+  const values = [`${limit}`];
+  const queryString = `SELECT * FROM properties LIMIT $1`;
+  return pool.query(queryString, values)
+    .then(res => res.rows)
+    .catch(e => console.log(e));
+  // for (let i = 1; i <= limit; i++) {
+  //   limitedProperties[i] = properties[i];
+  // }
+  // return Promise.resolve(limitedProperties);
 }
 exports.getAllProperties = getAllProperties;
 
